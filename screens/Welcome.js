211bb25bf4d256ed9006faf1f5209c07b854b405
KcSwornim <np03cs4a220169@heraldcollege.edu.np>
2025-03-02 10:17:42 +0545

    Added and updated JS files

---
+++ screens/Welcome.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import {
+  Avatar,
+  WelcomeImage,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  StyledButton,
+  InnerContainer,
+  WelcomeContainer,
+  ButtonText,
+  Line,
+  Colors,
+} from './../components/styles';
+const Welcome = ({navigation}) => {
+  return (
+    <>
+      <StatusBar style="light" />
+      <InnerContainer>
+        <WelcomeImage resizeMode="cover" source={require('./../assets/img/Img2.jpg')} />
+
+        <WelcomeContainer>
+          <PageTitle welcome={true}>Welcome! Traveller</PageTitle>
+          <SubTitle welcome={true}>Swornim KC</SubTitle>
+          <SubTitle welcome={true}>swornimkc@gmail.com</SubTitle>
+
+          <StyledFormArea>
+            <Avatar resizeMode="cover" source={require('./../assets/img/logo.jpg')} />
+
+            <Line />
+            <StyledButton onPress={() => {navigation.navigate('Login');
+
+            }}>
+              <ButtonText>Logout</ButtonText>
+            </StyledButton>
+          </StyledFormArea>
+        </WelcomeContainer>
+      </InnerContainer>
+    </>
+  );
+};
+
+export default Welcome;
