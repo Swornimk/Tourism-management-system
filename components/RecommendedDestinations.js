181d1f4ca0f158858e62ea981a55e4afbe485554
Swornimk <swornimkc12@gmail.com>
2025-05-26 17:10:51 +0545

    updated frontend

--- components/RecommendedDestinations.js
+++ components/RecommendedDestinations.js
@@ -214,7 +214,7 @@ const RecommendedDestinations = ({ navigation }) => {
 
   return (
     <View style={styles.section}>
-      <Text style={styles.sectionTitle}>Recommended For You</Text>
+      <Text style={styles.sectionTitle}>Destinations</Text>
       
       {recommendedTrips.map((trip) => (
         <TouchableOpacity 
